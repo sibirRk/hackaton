87863928b1dd9eb23ffd0fdf9a278c5f378fdbfe
Ruslan <degtrs@gmail.com>
2021-06-19 15:50:14 +0300

    no conflict

--- pages/sport/_code.vue
+++ pages/sport/_code.vue
@@ -1,5 +1,12 @@
 <template>
 <div class="sport-detail">
+  <div class="video__wrapper">
+    <video src="" class="video"></video>
+    <div class="video__info">
+      <h1 class="video__title"></h1>
+      <p class="video__description"></p>
+    </div>
+  </div>
 </div>
 </template>
 
