0bb8147e74495780bcf2f11b734f484f7973b4fe
AntoshJkeee <toha_bart@mail.ru>
2021-06-19 21:10:24 +0300

    компонент ListItem

---
+++ components/ListItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="list-item">
+    <div class="image-wrapper">
+      <img :src="image" alt="" />
+    </div>
+    <div class="text-content">
+      <div class="title-block">
+        <span class="title"> {{ title }} </span>
+      </div>
+      <span class="description">{{ description }}</span>
+      <el-tag>от {{ price }} ₽/час</el-tag>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListItem",
+
+  props: {
+    image: {
+      type: String
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String
+    },
+    price: {
+      type: Number
+    }
+  }
+};
+</script>
+
+<style lang="scss"></style>
