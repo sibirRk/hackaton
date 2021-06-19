bb2cdb58090160e554ff3ca78a0e2fb70a6808ae
Ruslan <degtrs@gmail.com>
2021-06-19 14:20:36 +0300

    changes

--- components/SportCard.vue
+++ components/SportCard.vue
@@ -1,11 +1,44 @@
 <template>
-  <div class="sport-card">
-    <div class="sport-card"></div>
-  </div>
+  <nuxt-link to="/" class="sport-card">
+    <img :src="item.img" class="sport-card__img">
+    <span class="sport-card__label">{{ item.label }}</span>
+  </nuxt-link>
 </template>
 
 <script>
 export default {
   name: 'SportCard',
+
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    }
+  },
 }
 </script>
+
+<style lang="scss">
+.sport-card {
+  display: flex;
+  flex-direction: column;
+  background: #F0F1F3;
+  border-radius: 4px;
+  width: 100%;
+  text-decoration: none;
+  padding-bottom: 16px;
+
+  &__img {
+    margin-bottom: 28px;
+  }
+
+  &__label {
+    color: #303133;
+    font-size: 16px;
+    line-height: 20px;
+    font-weight: bold;
+    text-align: center;
+    border-bottom: unset;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,20 +7,64 @@
       v-model="searchText"
       size="normal" clearable
     />
+
+    <div class="main-page__cards">
+      <sport-card
+        v-for="sport, index in sports"
+        :key="index"
+        :item="sport"
+      />
+    </div>
   </div>
   
 </template>
 
 <script>
+import SportCard from '@/components/SportCard.vue';
+
 export default {
   data() {
     return {
       searchText: '',
+      sports: [
+        {
+          img: '/images/sports/basketball.png',
+          label: 'Баскетбол',
+        },
+        {
+          img: '/images/sports/basketball.png',
+          label: 'Бадминтон',
+        },
+        {
+          img: '/images/sports/basketball.png',
+          label: 'Бокс',
+        },
+        {
+          img: '/images/sports/basketball.png',
+          label: 'Волейбол',
+        },
+        {
+          img: '/images/sports/basketball.png',
+          label: 'Бейсбол',
+        },
+        {
+          img: '/images/sports/basketball.png',
+          label: 'Баскетбол',
+        },
+        {
+          img: '/images/sports/basketball.png',
+          label: 'Кёрлинг',
+        },
+      ]
     }
   },
 
   async created() {
-    // const a = await this.$axios.get('http://sport-advisor.shahruslan.ru/api/sports');
+    const a = await this.$axios.get('http://sport-advisor.shahruslan.ru/api/sports');
+  },
+
+  components: {
+    SportCard,
   }
 }
 </script>
@@ -29,16 +73,29 @@ export default {
 .main-page {
   padding: 63px 16px 0;
 
-  .main-page__search {
+  &__search {
     input {
       border: 1px solid #DCDFE6;
       box-sizing: border-box;
       border-radius: 4px;
       // color: $text-main-color;
-      color: #606266;
-      font-weight: 500;
-      font-family: Inter, sans-serif;
+      margin-bottom: 16px;
+
+      &::placeholder {
+        color: #606266;
+        font-weight: 500;
+      }
     }
+
+    .el-input__icon {
+      color: #606266;
+    }
+  }
+
+  &__cards {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 15px;
   }
 }
 </style>
